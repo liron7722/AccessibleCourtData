0c4a6a368df6f28f532166392af2347b44bf7a3b
baruh <baruh2009@gmail.com>
2019-12-12 15:18:56 +0200

    Added new script and bugFix

--- scripts/checking_files_in_a_folder.py
+++ scripts/checking_files_in_a_folder.py
@@ -1,19 +1,19 @@
-import os
 from move_treated_files_to_a_new_location import *
 from index_data import *
 
-UNHANDLED_CSV_PRODUCTS_PATH = "C:/Users/Administrator/Documents/AccessibleCourtData/products/unhandled_csv_products"
+UNHANDLED_CSV_PRODUCTS_PATH = "products\/unhandled_csv_products"
 
 
 def Checking_files_in_a_folder():
-    directory = os.path.join(UNHANDLED_CSV_PRODUCTS_PATH)
+    directory = get_path(UNHANDLED_CSV_PRODUCTS_PATH)
+    print(directory)
     for root, dirs, files in os.walk(directory):
         for file in files:
             if file.endswith(".csv"):
                 ack = False
                 while ack != True:
                     ack = check_index(files_to_read=file, index_of_product=RULING_INDEX)
-                move_to_a_new_location(UNHANDLED_CSV_PRODUCTS_PATH + file)
+                move_to_a_new_location(os.path.join(directory, file))
 
 
 if __name__ == '__main__':
