29f7ef43929407627ce2eecd6209d3cff8793b4e
liron7722 <33122702+liron7722@users.noreply.github.com>
2020-03-19 20:55:34 +0200

    Update extra.py
    fix for linux

--- web_crawler/extra.py
+++ web_crawler/extra.py
@@ -12,14 +12,14 @@ def callSleep(days=1, hours=1, minutes=1, seconds=60):
 
 
 def readJson(path, filename, side=os.sep):
-    with open(path + side + filename) as json_file:
+    with open(path + side + filename, encoding='utf8') as json_file:
         data = json.load(json_file)
     return data
 
 
 def writeJson(path, filename, data, side=os.sep):
     with open(path + side + filename, 'w') as outfile:
-        json.dump(data, outfile, indent=4, ensure_ascii=False)
+        json.dump(data, outfile, indent=4, ensure_ascii=True)
 
 
 def separateDate(date):
