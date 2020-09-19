2ddd7c167c0f38e1053c62502d03329bac137310
liron7722 <liron7722@gmail.com>
2020-09-19 12:58:16 +0300

    Update Linker.py
    removing un needed functions
    add downloaded cases counter function for manual use

--- ILCourtScraper/Scrapers/Linker.py
+++ ILCourtScraper/Scrapers/Linker.py
@@ -1,12 +1,14 @@
 from ILCourtScraper.Extra.time import *
-from ILCourtScraper.Extra.path import *
-from ILCourtScraper.Extra.json import saveData, readData
+
+dateURL_P1 = 'https://supreme.court.gov.il/Pages/SearchJudgments.aspx?&OpenYearDate=null&CaseNumber=null&DateType=2&SearchPeriod=null&COpenDate='
+dateURL_P2 = '&CEndDate='
+dateURL_P3 = '&freeText=null&Importance=null'
 
 
 # Functions
 # input - startDate as string, format as string
 # do - create list of dict contain {'date' as string, 'first' as int, 'last' as int, 'is taken' as boolean}
-def createDates(startDate=None, format="%d-%m-%Y"):
+def createDates(startDate=None, fmt="%d-%m-%Y"):
     dates = list()
 
     if startDate is None:  # no endDate input so we use today date
@@ -14,31 +16,13 @@ def createDates(startDate=None, format="%d-%m-%Y"):
     else:
         sD = startDate.replace('/', '-')
 
-    eD = strftime(format, localtime())
+    eD = strftime(fmt, localtime())
 
-    for date in generateDates(startDate=sD, endDate=eD, format=format):
+    for date in generateDates(startDate=sD, endDate=eD, fmt=fmt):
         strDate = str(date).replace('-', '/')
         item = {'date': strDate, 'first': 0, 'last': -1, 'is taken': False, 'case List': []}
         dates.append(item)
     return dates
-
-
-# do - make sure local file is up to date, if not not exist create one
-def upToDateFile():
-    if path.isfile(filePath + sep + DateListFileName):  # check file exist
-        datesList = readData(DateListFileName, filePath)
-        if len(datesList) > 0:
-            item = datesList[-1]  # get the last day that scraped,
-            new_dates = createDates(startDate=item['date'])
-        else:
-            new_dates = createDates()
-
-        if len(new_dates) > 0:
-            datesList.extend(new_dates)
-
-    else:  # local file doesn't exist
-        datesList = createDates()
-    saveData(datesList, DateListFileName, filePath)  # create\update local file
 
 
 def upToDateDB(db=None):
@@ -49,61 +33,11 @@ def upToDateDB(db=None):
         for item in query:
             item.pop('_id')
             datesList.append(item)
-
-        new_dates = createDates(startDate=datesList[-1]['date'])
-        datesList.extend(new_dates)
+        new_dates = createDates(startDate=datesList[-1]['date']) if len(datesList) > 0 else createDates()
 
         for item in new_dates:
             collection = db.get_collection('dates')
             collection.insert_one(item)
-
-        saveData(datesList, DateListFileName, filePath)
-
-
-# output - return list of links as list({'date': string, first': int, 'last': int, 'is taken': boolean})
-def getLinks(db=None):
-    if db is not None:  # use db
-        upToDateDB(db)
-        collection = db.get_collection('dates')
-        dateList = list(collection.find({'is taken': False}).skip(0))
-        if len(dateList) > 0:
-            item = dateList[-1]  # last item from non taken dates
-            UpdateScrapeList(db, item['date'], item['first'], item['last'], True, item['case List'])
-            return item
-        else:
-            resetDatesInDB(db)
-            return getLinks(db)
-
-    else:  # use file
-        upToDateFile()
-        datesList = readData(DateListFileName, filePath)
-        while True:
-            for item in datesList:
-                if item['is taken'] == False:
-                    return item
-
-
-# input - db as database, date as string, first as int, last as int, status as boolean
-# do - update all function relate to the list
-def UpdateScrapeList(db, date, first, last, status, case_List):
-    result = []
-    for func in updateFunction:
-        res = func(db, date, first, last, status, case_List)
-        result.append(res)
-    return result
-
-
-# input - date as string, first as int, last as int, status as boolean
-def updateDateInFile(db, date, first, last, status, case_List):
-    datesList = readData(DateListFileName, filePath)
-    for item in datesList:
-        if item['date'] == date:
-            item['first'] = first
-            item['last'] = last
-            item['is taken'] = status
-            item['case List'] = case_List
-    saveData(datesList, DateListFileName, filePath)
-    return True
 
 
 # input - db as database, date as string, first as int, last as int, status as boolean
@@ -111,7 +45,6 @@ def updateDateInDB(db, date, first, last, status, case_List):
     collection = db.get_collection('dates')
     collection.update_one({'date': date},
                           {"$set": {'first': first, 'last': last, 'is taken': status, 'case List': case_List}})
-    return True
 
 
 def resetDatesInDB(db):
@@ -119,15 +52,37 @@ def resetDatesInDB(db):
     collection.update_many({}, {'$set': {'is taken': False}})
 
 
-dateURL_P1 = 'https://supreme.court.gov.il/Pages/SearchJudgments.aspx?&OpenYearDate=null&CaseNumber=null&DateType=2&SearchPeriod=null&COpenDate='
-dateURL_P2 = '&CEndDate='
-dateURL_P3 = '&freeText=null&Importance=null'
+# output - return list of links as list contain:
+#                           ({'date': string, first': int, 'last': int, 'is taken': boolean, 'case List': list})
+def getLinks(db):
+    if db is not None:  # use db
+        upToDateDB(db)
+        collection = db.get_collection('dates')
+        dateList = list(collection.find({'is taken': False}).skip(0))
+        if len(dateList) > 0:
+            item = dateList[-1]  # last item from non taken dates
+            updateDateInDB(db, item['date'], item['first'], item['last'], True, item['case List'])
+            return item
+        else:
+            resetDatesInDB(db)
+            return getLinks(db)
+    else:
+        print("Got None as db value")
 
-filePath = getPath(N=0) + f'products{sep}'
-DateListFileName = 'dateList.json'
 
-updateFunction = [updateDateInFile, updateDateInDB]
+def countStatus(db):
+    count, N = 0, 0
+    collection = db.get_collection('dates')
+    cursor = collection.find({})
+    for item in cursor:
+        N += item['last']
+        count += item['last'] - len(item['case List'])
+    print(f'{count} of {N} files ware downloaded')
+    print('This is {:.2f}% of all cases'.format(count / N * 100))
 
-#from ILCourtScraper.Extra.db import DB
-#db = DB().getDB('SupremeCourt')
-#resetDatesInDB(db)
+
+# For manual reset or count
+# from ILCourtScraper.Extra.db import DB
+# db = DB().getDB('SupremeCourt')
+# resetDatesInDB(db)
+# countStatus(db)
